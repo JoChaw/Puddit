09b2fabc985afba351f4508f5d84ac4f02604a2b
JonShepChen <SharpShooter787@gmail.com>
2015-08-13 17:23:12 -0700

    Time Stamp Element Code

--- puddit/app.py
+++ puddit/app.py
@@ -1,10 +1,11 @@
 import requests
 import json
+import time
 from bs4 import BeautifulSoup
 
 #Which subreddits would you like to monitor? (subreddits max)
 
-target_subreddit = 'teslamotors'
+target_subreddit = 'askreddit'
 pb_request_url = 'https://api.pushbullet.com/v2/pushes'
 content_type = 'application/json'
 authorization = 'Bearer v1s0QnhullE8cI6AB6fEwUpBIrEr2vPEGeujw1WWxXJy8'
@@ -21,12 +22,13 @@ reddit_response = requests.get(reddit_request_url, headers={'User-Agent': "Puddi
 reddit_response_text = reddit_response.text
 reddit_soup = BeautifulSoup(reddit_response_text)
 reddit_site_table = reddit_soup.find_all('a', 'title', 'href', text=True)
+reddit_time_table = reddit_soup.find_all('time', 'live-timestamp', text=True)
 
 r = requests.post(pb_request_url, headers=pb_request_headers, data=json.dumps(pb_request_body))
 
 
-for blah in reddit_site_table:
-    print(blah['href'] + blah.contents[0])
+for blah, grah in zip(reddit_site_table, reddit_time_table):
+    print(blah['href'] + blah.contents[0] + grah.contents[0])
 
 
 #validate subreddit
